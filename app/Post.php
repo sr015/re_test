2e1c841e31507a8b991887e5bb11b34db0735f3d
sr015 <c1118829@planet.kanazawa-it.ac.jp>
2022-06-25 18:23:27 +0900

    アウトプット課題3終了

--- app/Post.php
+++ app/Post.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     
-    public function getPaginateByLimit(int $limit_count=10)
+    public function getByLimit(int $limit_count=10)
     {
         // updated_atで降順に並べた後、limitで件数制限をかける
-        return $this ->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this ->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
 }
