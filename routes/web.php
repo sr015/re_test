8599a78c104c980fed7782d77fbe510431898228
sr015 <c1118829@planet.kanazawa-it.ac.jp>
2022-06-29 15:14:14 +0900

    08-5できた

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', function() {
 });
 */
 Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
-;
+Route::post('/posts', 'PostController@store');
